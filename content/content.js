f492db7a7505b0a47ba7c8339b4bcbd52180a3ba
dekuyou <dekuyou@gmail.com>
2015-04-09 23:43:42 +0900

    表示しているテキストエリアのみ対象とするように変更

--- content/content.js
+++ content/content.js
@@ -1,5 +1,5 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    var oldMessage = $("textarea[name='note[note]']").val();
-    $("textarea[name='note[note]']").val(oldMessage + "\n\n" + request.image);
+    var oldMessage = $("textarea[name='note[note]']:visible").val();
+    $("textarea[name='note[note]']:visible").val(oldMessage + "\n\n" + request.image);
 });
